8daca12c38b1d461b84fb321d2d2c07aacc75dbd
twismon <twismon@gmail.com>
2015-06-13 21:13:38 +0800

    Console
    基本Console指令

---
+++ HW1_Console/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string Name, Gender;
+            Console.WriteLine("請您輸入姓名:");
+            Name = Console.ReadLine();
+            Console.WriteLine("請您輸入性別, 男或女?");
+            Gender = Console.ReadLine();
+            Console.WriteLine("{0}您好!!", Name);
+            Console.WriteLine("我猜{0}您一定是{1}生", Name, Gender);
+        }
+    }
+}
